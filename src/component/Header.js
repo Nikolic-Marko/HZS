936d53277eb9f02890545ec9a8231adac2c46e6b
Nikolic-Marko <nikolicmarko653@gmail.com>
2021-01-26 19:25:48 +0100

    Seting up project

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <p>ZELIM VAM SVIMA SRECAN POCETAK RADA! </p>
+            </div>
+        );
+    }
+}
+
+export default Header;
